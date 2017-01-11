d35694578830476ecaca6cc82b24b9f5e14bf02c
Andy Blankfield <blankfieldatwork@gmail.com>
2017-01-11 15:35:28 -0700

    Initial commit with game plan.

---
+++ fresh_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# This script is for setting up a fresh copy of debian net install
+# It installs the items I will need to start coding in debian
---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This is a setup script for the dot files in this directory.
+# It will also be used for triggering a script for setting up a fresh debian net install
+
+
+# Check if dot files exist
+# Move any existing dot files to file.bak
+# Move dot files to home
+
+
+# trigger for fresh_setup.sh
